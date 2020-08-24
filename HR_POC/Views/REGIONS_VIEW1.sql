fb64c3078d644bdf2cc34abdcae859f5de7c7551
shaikhmohdjaffer <shaikhmohdjaffer@gmail.com>
2020-08-24 08:11:18 +0000

    Regions_view1 created

--- HR_POC/Views/REGIONS_VIEW1.sql
+++ HR_POC/Views/REGIONS_VIEW1.sql
@@ -1,2 +1,2 @@
 CREATE OR REPLACE FORCE VIEW hr_poc.regions_view1 (region_id,region_name,region_code1) AS
-SELECT "REGION_ID","REGION_NAME","REGION_CODE1" FROM REGIONS;
+select "REGION_ID","REGION_NAME","REGION_CODE1" from regions;
